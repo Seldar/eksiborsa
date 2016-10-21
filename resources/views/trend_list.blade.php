7d73c9a4551bf1977b74a5d7f8f68f912bef7f96
Volkan Ulukut <arthan@gmail.com>
2016-10-21 17:50:16 +0300

    fix highcharts css

--- resources/views/trend_list.blade.php
+++ resources/views/trend_list.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <script src="../node_modules/highcharts/js/highcharts.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="../node_modules/highcharts/css/highcharts.css" type="text/css">
 <div class="container">
     <div id="container" style="width:100%; height:400px;"></div>
 </div>
